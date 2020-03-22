cb0bf19bd1536443b62d31b024980d73ac098269
Connor Claxton <czclaxton@gmail.com>
2020-03-22 18:16:38 -0500

    added comment

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -2,6 +2,7 @@ export const STAGE_WIDTH = 12;
 export const STAGE_HEIGHT = 20;
 
 export const createStage = () =>
+  // Create nested arrays to represent placeholders for the grid
   Array.from(Array(STAGE_HEIGHT), () =>
     new Array(STAGE_WIDTH).fill([0, "clear"])
   );
